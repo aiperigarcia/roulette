fa82066eabe299d906cd812ba750772b99e8d2d8
aiperieldar <aiperieldar.dev@gmail.com>
2020-03-27 20:41:27 -0400

    Initial Commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+
+module.exports = {
+
+    'url' : 'mongodb+srv://rc:123@totals-szgz4.mongodb.net/roulette?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'roulette'
+};
